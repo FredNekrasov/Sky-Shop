085d84d4cb94d242b5b73472ad685932d87f50b1
FredNekrasov <frednekrasov49@gmail.com>
2025-01-14 00:15:09 +0300

    creating DTOs

---
+++ src/SkyShop.DAL/FakeRemote/DTOs/AttractionDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace SkyShop.DAL.FakeRemote.DTOs;
+
+public class AttractionDTO
+{
+    public int Id { get; set; }
+    public string PhotoURL { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+}
---
+++ src/SkyShop.DAL/FakeRemote/DTOs/TicketDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace SkyShop.DAL.FakeRemote.DTOs;
+
+public class TicketDTO
+{
+    public string StartCity { get; set; } = string.Empty;
+    public string StartCityCode { get; set; } = string.Empty;
+    public string EndCity { get; set; } = string.Empty;
+    public string EndCityCode { get; set; } = string.Empty;
+    public string StartDate { get; set; } = string.Empty;
+    public string EndDate { get; set; } = string.Empty;
+    public int Price { get; set; }
+    public string SearchToken { get; set; } = string.Empty;
+}

3f4a5f6d65873066dcf5825d29400e238c359737
FredNekrasov <frednekrasov49@gmail.com>
2025-01-14 00:19:12 +0300

    adding photos and creating a class that returns fake data about attractions

---
+++ src/SkyShop.DAL/FakeRemote/Attractions/AttractionsAPI.cs
@@ -0,0 +1,14 @@
+﻿using SkyShop.DAL.FakeRemote.DTOs;
+using System.Collections.Immutable;
+using System.Text.Json;
+
+namespace SkyShop.DAL.FakeRemote.Attractions;
+
+public class AttractionsAPI
+{
+    public static ImmutableList<AttractionDTO> GetAttractions()
+    {
+        var attractions = File.ReadAllText("AttractionsSource.json");
+        return JsonSerializer.Deserialize<List<AttractionDTO>>(attractions)?.ToImmutableList() ?? [];
+    }
+}

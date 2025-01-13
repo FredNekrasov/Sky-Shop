c42ac2bf86c6165eef70be1f3ffbb34897b6cfd5
FredNekrasov <frednekrasov49@gmail.com>
2025-01-13 19:03:33 +0300

    creating models

---
+++ src/SkyShop.DAL/Database/Entities/Ticket.cs
@@ -0,0 +1,13 @@
+﻿namespace SkyShop.DAL.Database.Entities;
+
+public class Ticket
+{
+    public int Id { get; set; }
+    public string StartCity { get; set; } = string.Empty;
+    public string StartCityCode { get; set; } = string.Empty;
+    public string EndCity { get; set; } = string.Empty;
+    public string EndCityCode { get; set; } = string.Empty;
+    public DateTime StartDate { get; set; } = DateTime.MinValue;
+    public DateTime EndDate { get; set; } = DateTime.MinValue;
+    public int Price { get; set; }
+}
---
+++ src/SkyShop.DAL/Database/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SkyShop.DAL.Database.Entities;
+
+[PrimaryKey(nameof(Id))]
+public class User
+{
+    public int Id { get; set; }
+    public string UserName { get; set; } = string.Empty;
+    public DateTime Birthday { get; set; } = DateTime.Now!;
+    public string Password { get; set; } = string.Empty;
+    public string Mail { get; set; } = string.Empty;
+    public bool IsActive { get; set; }
+    public Profile Profile { get; set; } = null!;
+}
